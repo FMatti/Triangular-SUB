00d5616c60d5ebbefec21cd25b9cc966eca8ea50
FMatti <somecallmefabio@gmail.com>
2022-08-14 08:06:11 +0200

    change notation in substitution function

--- sub.py
+++ sub.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def sub(A: np.ndarray, b: np.ndarray, type: str = 'lower') -> np.ndarray:
+def sub(A: np.ndarray, b: np.ndarray, mode: str = 'lower') -> np.ndarray:
     """
     Solve Ax = b for triangular A with forward/backward substitution
     
@@ -8,7 +8,7 @@ def sub(A: np.ndarray, b: np.ndarray, type: str = 'lower') -> np.ndarray:
     ----------
     A : np.ndarray, shape (M, M)
     b : np.ndarray, shape (M,)
-    type : str, default is 'lower'
+    mode : str, default is 'lower'
         'lower' -> if A is lower triangular (forward substitution)
         'upper' -> if A is upper triangular (backward substitution)
 
@@ -18,7 +18,7 @@ def sub(A: np.ndarray, b: np.ndarray, type: str = 'lower') -> np.ndarray:
     """
     x = np.zeros_like(b)
 
-    for i in range(len(b)) if type == 'lower' else reversed(range(len(b))):
-        x[i] = (b[i] - np.dot(A[i, :], x[:])) / A[i, i]
+    for i in range(len(b)) if mode == 'lower' else reversed(range(len(b))):
+        x[i] = (b[i] - np.dot(A[i], x[:])) / A[i, i]
 
     return x
